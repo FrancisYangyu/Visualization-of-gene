ea447ecce4bd661c09105eb547690ee05a529c9f
Francis <Francisy@shu.edu.cn>
2018-04-05 09:04:51 +0800

    some changes
    The first verison of  visualization of gene by using d3.js and its zoom funciton

--- store.js
+++ store.js
@@ -7,6 +7,7 @@
 //    ctx.fillRect(100, 100, 200, 200);
 //}
 var gene = "CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACCCACACACACACATCCTAACACTACCCTAACACAGCCCTAATCTAACCCTGGCCAACCTGTCTCTCAACTTACCCTCCATTACCCTGCCTCCACTCGTTACCCTGTCCCATTCAACCATACCACTCCGAACCACCATCCATCCCTCTACTTACTACCACTCACCCACCGTTACCCTCCAATTACCCATATCCAACCCACTGCCACTTACCCTACCATTACCCTACCATCCACCATGACCTACTCACCATACTGTTCTTCTACCCACCATATTGAAACGCTAACAAATGATCGTAAATAACACACACGTGCTTACCCTACCACTTTATACCACCACCACATGCCATACTCACCCTCACTTGTATACTGATTTTACGTACGCACACGGATGCTACAGTATATACCATCTCAAACTTACCCTACTCTCAGATTCCACTTCACTCCATGGCCCATCTCTCACTGAATCAGTACCAAATGCACTCACATCATTATGCACGGCACTTGCCTCAGCGGTCTATACCCTGTGCCATTTACCCATAACGCCCATCATTATCCACATTTTGATATCTATATCTCATTCGGCGGTCCCAAATATTGTATAACTGCCCTTAATACATACGTTATACCACTTTTGCACCATATACTTACCACTCCATTTATATACACTTATGTCAATATTACAGAAAAATCCCCACAAAAATCACCTAAACATAAAAATATTCTACTTTTCAACAATAATACATAAACATATTGGCTTGTGGTAGCAACACTATCATGGTATCACTAACGTAAAAGTTCCTCAATATTGCAATTTGCTTGAACGGATGCTATTT";
+//console.log(gene.length);
 
 //键值对 碱基和对应颜色
 var baseOp = {
@@ -14,19 +15,14 @@ var baseOp = {
     'G': 'C',
     'C': 'G',
     'A': 'T'
-};
+}
 var colorRange = {
-    'A': '#0099CC', //blue
-    'C': '#66CC66', //green
-    'G': '#CC99CC', //purple
-    'T': '#FF6666' //red
+    'A': 'blue',
+    'C': 'green',
+    'G': 'purple',
+    'T': 'red'
 };
-//var colorRange = {
-//    'A': 'blue', //blue
-//    'C': 'green', //green
-//    'G': 'purple', //purple
-//    'T': 'red' //red
-//};
+
 //the size of web  changing lead to sth
 //window.onresize = function () {
 //    getWinSize();
@@ -35,15 +31,15 @@ var colorRange = {
 //    loopDraw();
 //    location.reload();
 //}
-$(function() {
-    $(window).resize(function() {
+$(function () {
+    $(window).resize(function () {
         getWinSize();
         svgContainer.attr("width", winWidth)
             .attr("height", 3 * baseSize);
         loopDraw();
         location.reload();
-    });
-});
+    })
+})
 var winHeight, winWidth;
 //get the width and height of the window
 function getWinSize() {
@@ -54,10 +50,13 @@ function getWinSize() {
 //create the area of D3 SVG
 function createD3svg() {
     getWinSize();
-    //var svg = d3.select("body").append("g").call(zoom);
+    //var svgContainer = d3.select("body").append("g").call(zoom);
     var svgContainer = d3.select("body").append("svg")
         .attr("width", winWidth)
-        .attr("height", winHeight);
+        .attr("height", 3 * baseSize)
+        .append("g")
+        .call(zooming())
+        .append("g");
     return svgContainer;
 }
 
@@ -65,7 +64,7 @@ function createD3svg() {
 function drawRect(svgContainer, x, y, baseSize, base, type) {
     if (type == 0) {
         base = baseOp[base];
-        y = y + baseSize + 3;
+        y = y + baseSize + 5;
     }
     svgContainer.append("rect")
         .attr("x", x)
@@ -73,41 +72,45 @@ function drawRect(svgContainer, x, y, baseSize, base, type) {
         .attr("width", baseSize)
         .attr("height", baseSize)
         .attr("fill", colorRange[base])
-        .on("mouseover", function() {
-            d3.select(this).style('fill', "yellow");
+        .on("mouseover", function () {
+            d3.select(this).style('fill', "yellow")
         })
-        .on("mouseout", function() {
-            d3.select(this).transition().duration(200).style('fill', colorRange[base]);
+        .on("mouseout", function () {
+            d3.select(this).transition().duration(200).style('fill', colorRange[base])
         });
-    svgContainer.append('text').text(base).attr('fill', 'white')
-        .attr('x', x + baseSize / 2)
-        .attr('y', y + baseSize / 2)
-        .attr('text-anchor', 'middle')
-        .style('font-size', baseSize / 2 + 'px')
-        .attr('dy', 8);
 }
 // draw many rects
 function loopDraw() {
     for (var i = 0, x = 0, y = 0; i < gene.length /*i < winWidth / (baseSize + 1) - 2*/ ; i++, x += 2 + baseSize) {
         var point = i + beginPoint;
         //var point = i;
-        drawRect(g, x, y, baseSize, gene[point], 1);
-        drawRect(g, x, y, baseSize, gene[point], 0);
+        drawRect(svgContainer, x, y, baseSize, gene[point], 1);
+        drawRect(svgContainer, x, y, baseSize, gene[point], 0);
     }
 }
-var baseSize = 50; //the picture size of each base group
-var beginPoint = 0;
-var svgContainer = createD3svg();
-var g = svgContainer.append("g");
-loopDraw();
-svgContainer.call(d3.zoom()
-    .scaleExtent([1 / 100, 4])
-    .on("zoom", zoomed));
-
-function zoomed() {
-    g.attr("transform", d3.event.transform); //
+//add the A T C G to every rect
+//not completed !!
+function addText(base) {
+    var text = svgContainer.append("text").attr("fill", "white")
+        .attr("font-size", baseSize + "px")
+        .attr("text-anchor", "middle")
+        .text(base);
 }
-// svgContainer.call(d3.selectAll("svg").call(d3.drag().on("start", started)));
-//svgheight= webheigh/5; svg
-//pointOne=1000, pointTwo=100;
-//baseSize= svgheighy/gene.lengh;
+
+function scaling() {
+    alert("123");
+    beginPoint -= 100;
+}
+
+function zooming() {
+    var zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+
+    function zoomed() {
+        svgContainer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+    return zoom;
+}
+var svgContainer = createD3svg();
+var baseSize = 50; //the picture size of each base group
+var beginPoint = 300;
+loopDraw();
